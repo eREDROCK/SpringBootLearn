d99a3e6c5d0b5debcc42f94c5804ca15bf535781
eREDROCK <rdrc136@gmail.com>
2023-07-21 19:34:19 +0900

    [Fix]色々変更した

--- docker/mysql/sql_init/02_initial-data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO sample_schema.account (email, password, user_name) VALUES
-('mng@hoge', 'mngpass', 'manager'),
-('user1@hoge', 'user1pass', 'MR.user1'),
-('user2@hoge', 'user2pass', 'MR.user2')
-;

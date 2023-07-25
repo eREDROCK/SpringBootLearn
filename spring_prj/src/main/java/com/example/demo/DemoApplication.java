e98e65ac4d70894029b740287e325ef6fd1c8c67
eREDROCK <rdrc136@gmail.com>
2023-07-25 18:14:54 +0900

    [Add]hello.htmlの表示に失敗

--- spring_prj/src/main/java/com/example/demo/DemoApplication.java
+++ spring_prj/src/main/java/com/example/demo/DemoApplication.java
@@ -4,14 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.hello")
 @RestController
 public class DemoApplication {
 
 	@RequestMapping("/")
 	public String home(){
-		return "Hello World!!";
+		return "index画面です.";
 	}
 
 	public static void main(String[] args) {

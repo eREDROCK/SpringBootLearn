ab52756bf045a6ef2bd2b6d712b3c75346baf58c
eREDROCK <rdrc136@gmail.com>
2023-09-04 15:42:20 +0900

    gitignoreの変更

---
+++ initdb/init.sql
@@ -0,0 +1,10 @@
+INSERT INTO `taskdb`.`users`
+(`id`,
+`email`,
+`name`,
+`password`)
+VALUES
+(1,
+'test@test.jp',
+'テスト太郎',
+'password');
--- spring_prj/src/main/java/com/example/demo/DemoApplication.java
+++ spring_prj/src/main/java/com/example/demo/DemoApplication.java
@@ -5,14 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
+	@Controller
 	@RequestMapping("/")
-	public String home(){
-		return "index画面です.";
+	public class IndexController {
+
+			@GetMapping
+			public String index(Model model) {
+					return "pokecalist/index";
+			}
 	}
 
 	public static void main(String[] args) {

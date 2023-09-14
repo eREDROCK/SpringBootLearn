62b9fc5ba4557aa8056fc66ad51d5e59336a6dd2
eREDROCK <rdrc136@gmail.com>
2023-09-14 15:48:44 +0900

    [Add]サインアップ画面の追加

---
+++ spring_prj/src/main/java/com/example/demo/controller/RegisterController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public String index(Model model) {
+        return "pokecalist/register";
+    }
+}

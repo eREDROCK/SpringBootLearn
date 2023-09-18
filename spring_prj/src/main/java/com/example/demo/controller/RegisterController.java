0871d05dc449343b4e6d94af164dd5b189da5357
eREDROCK <rdrc136@gmail.com>
2023-09-18 14:17:41 +0900

    [Add]Contorollerクラスの編集

--- spring_prj/src/main/java/com/example/demo/controller/RegisterController.java
+++ spring_prj/src/main/java/com/example/demo/controller/RegisterController.java
@@ -1,26 +1,33 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
+import com.example.demo.request.UserRequest;
+
 
 @Controller
-@RequestMapping("/register")
-public class RegisterController {
+public class RegisterController{
     @Autowired
     private UserService userService;
 
-    @GetMapping
-    public String index(Model model) {
-        List<User> users = userService.searchAll();
-        model.addAttribute("users", users);
-        return "pokecalist/register";
+    @GetMapping(value="/register")
+    public void displayRegister({
+        return "pokecalist/register"
     }
+
+    
 }
